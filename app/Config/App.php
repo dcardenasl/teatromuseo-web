f93765b49e6d92d7ccff18aa552867b16a4fcd62
David Cardenas Lorca <cine.dcl@gmail.com>
2026-07-28 03:10:41 -0400

    chore(i18n): add fr/pt to fallback supported locales list

--- app/Config/App.php
+++ app/Config/App.php
@@ -126,7 +126,7 @@ class App extends BaseConfig
      *
      * @var list<string>
      */
-    public array $supportedLocales = ['es', 'en'];
+    public array $supportedLocales = ['es', 'en', 'fr', 'pt'];
 
     /**
      * --------------------------------------------------------------------------
