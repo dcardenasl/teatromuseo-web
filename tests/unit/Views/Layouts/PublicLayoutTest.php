8efa737e2382bbb86e3d88c619ff1414ff389e39
David Cardenas Lorca <cine.dcl@gmail.com>
2026-08-16 12:27:47 -0400

    test(web): cover non-clickable menu items and accessible submenu toggle

--- tests/unit/Views/Layouts/PublicLayoutTest.php
+++ tests/unit/Views/Layouts/PublicLayoutTest.php
@@ -17,7 +17,7 @@ final class PublicLayoutTest extends CIUnitTestCase
             'view' => 'page',
             'mainMenu' => ['items' => []],
             'footerMenu' => ['name' => 'Menu variant', 'items' => [
-                ['label' => 'Test Link', 'custom_url' => '/test', 'children' => []],
+                ['label' => 'Test Link', 'custom_url' => '/test', 'is_clickable' => true, 'children' => []],
             ]],
             'legalMenu' => ['items' => []],
             'settings' => [
@@ -36,5 +36,53 @@ final class PublicLayoutTest extends CIUnitTestCase
         $this->assertStringContainsString('<span class="text-xl font-bold text-primary">Mi Sitio</span>', $html);
         $this->assertStringContainsString('>Menu variant</p>', $html);
         $this->assertStringNotContainsString('<style', $html);
+    }
+
+    public function testNonClickableMenuItemsRenderWithoutAnchorsOrHashFallbacks(): void
+    {
+        $html = view('layouts/public', [
+            'view' => 'page',
+            'mainMenu' => ['items' => [
+                [
+                    'id' => 'about',
+                    'label' => 'Nosotros',
+                    'custom_url' => null,
+                    'is_clickable' => false,
+                    'children' => [
+                        [
+                            'id' => 'history',
+                            'label' => 'Historia',
+                            'custom_url' => null,
+                            'is_clickable' => false,
+                            'children' => [],
+                        ],
+                        [
+                            'id' => 'contact',
+                            'label' => 'Contacto',
+                            'custom_url' => '/contacto',
+                            'is_clickable' => true,
+                            'children' => [],
+                        ],
+                    ],
+                ],
+                [
+                    'id' => 'empty',
+                    'label' => 'Sin enlace',
+                    'custom_url' => null,
+                    'is_clickable' => false,
+                    'children' => [],
+                ],
+            ]],
+            'footerMenu' => ['items' => []],
+            'legalMenu' => ['items' => []],
+            'settings' => [],
+        ]);
+
+        self::assertStringNotContainsString('href="#"', $html);
+        self::assertSame(0, preg_match_all('/<a[^>]*>\s*Sin enlace\s*<\/a>/u', $html));
+        self::assertSame(0, preg_match_all('/<a[^>]*>\s*Nosotros\s*<\/a>/u', $html));
+        self::assertStringContainsString('href="' . site_url('/es/contacto') . '"', $html);
+        self::assertStringContainsString('data-submenu-toggle', $html);
+        self::assertStringContainsString('aria-expanded="false"', $html);
     }
 }
